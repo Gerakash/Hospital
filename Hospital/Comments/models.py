d855f6136cb3e960d6205361e6c93f64701cfa05
gerakash <aigerim.kash@icloud.com>
2021-03-29 17:13:27 +0600

    comments files

---
+++ Hospital/Comments/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from Hospitals.models import Doctor
+
+
+class Comment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Comments'
+        verbose_name_plural = 'Comments'
